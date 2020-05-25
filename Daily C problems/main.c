ce42a7d0c5bf63cc8c1f0b6b199b1034667bc9bb
Aditya Kumar <53961324+AdityaKumar4@users.noreply.github.com>
2020-05-25 23:30:29 +0530

    Add files via upload

--- Daily C problems/main.c
+++ Daily C problems/main.c
@@ -1,25 +1,18 @@
+//
+//  main.c
+//  S8:Charcter to Number Conversion
+//
+//  Created by Aman Singh on 21/11/18.
+//  Copyright © 2018 Aman Singh. All rights reserved.
+//
+
 #include <stdio.h>
-#include <math.h>
-int convertBinaryToDecimal(long long n);
 
 int main()
 {
-    long long n;
-    printf("Enter a binary number: ");
-    scanf("%lld", &n);
-    printf("%lld in binary = %d in decimal", n, convertBinaryToDecimal(n));
+    char c;
+    printf("Enter the character :");
+    scanf("%c",&c);
+    printf("\n The numerical equivalent for the character %c is %d",c,c);
     return 0;
-}
-
-int convertBinaryToDecimal(long long n)
-{
-    int decimalNumber = 0, i = 0, remainder;
-    while (n!=0)
-    {
-        remainder = n%10;
-        n /= 10;
-        decimalNumber += remainder*pow(2,i);
-        ++i;
-    }
-    return decimalNumber;
 }
